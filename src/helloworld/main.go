fe97dede00840455f906e57e46aa55e1a1b2ac06
Capella87 <capella87@outlook.com>
2024-04-13 13:48:36 +0900

    practice(helloworld): constant declaration

--- src/helloworld/main.go
+++ src/helloworld/main.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+const pi = 3.1416
+
+const (
+	No                = !Yes
+	Yes               = true
+	MaxDegrees        = 360
+	Unit       string = "radian"
+	A, B              = int64(-3), int64(5)
+	Y                 = float32(2.718)
+	MaxUnit           = ^uint(0)
+)
+
 func main() {
-	fmt.Println("Hello World!")
+	var ima complex64 = 1.2i
+
+	fmt.Println(ima)
+	fmt.Println(pi)
 }
